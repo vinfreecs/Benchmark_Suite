acf52d7a1aabdc172d3739a3121088e06abb51a8
Vinay <89305950+vinfreecs@users.noreply.github.com>
2025-11-19 22:50:05 +0100

    command-line args as a string to benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,52 @@
 #include <iostream>
 #include <memory>
 #include <omp.h>
+#include <string>
 #include "timing.h"
 #include "vec.hpp"
 #include "kernals.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
 
-  auto a1 = new vec(10000);
-  auto a2 = new vec(10000);
-  auto a3 = new vec(10000);
+  if (argc != 2)
+  {
+    std::cout << argv[0] << " <kernal_name>" << std::endl;
+    std::cout << "Choose one of these kernals \n-> axpby\n-> dot " << std::endl;
+    return 1;
+  }
 
   // add test runs
   int warmupIter = 10;
   int iter = 100;
 
-  // A standard harness to time and can alter which kernal's benchmark to use at runtime
-  HARNESS(axpby(a3, a2, a1, 1, 2, 10000))
+  if (strcmp(argv[1], "axpby") == 0)
+  {
+    std::cout << "-- BENCHMARKING AXPBY --" << std::endl;
+    auto a1 = new vec(10000);
+    auto a2 = new vec(10000);
+    auto a3 = new vec(10000);
 
-  double dot_result = 0.0;
+    // A standard harness to time and can alter which kernal's benchmark to use at runtime
+    HARNESS(axpby(a3, a2, a1, 1, 2, 10000))
 
-  // HARNESS(dot(dot_result, a1, a2))
-
-  delete a1;
-  delete a2;
-  delete a3;
+    delete a1;
+    delete a2;
+    delete a3;
+  }
+  else if (strcmp(argv[1], "dot") == 0)
+  {
+    std::cout << "-- BENCHMARKING DOT --" << std::endl;
+    auto a1 = new vec(10000);
+    auto a2 = new vec(10000);
+    double dot_result = 0.0;
+    HARNESS(dot(dot_result, a1, a2))
+    delete a1;
+    delete a2;
+  }
+  else
+  {
+    std::cerr << "This is not available" << std::endl;
+  }
+  return 0;
 }
