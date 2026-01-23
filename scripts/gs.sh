e4d1e9ac283e207d7f536004ef72ed4dd7a3a2f7
Vinay Kumar Reddy Patil <89305950+vinfreecs@users.noreply.github.com>
2026-01-23 09:35:48 +0100

    Fixed cpu-frequency in *.sh

--- scripts/gs.sh
+++ scripts/gs.sh
@@ -6,7 +6,7 @@ cd .. && make clean && make
 echo "Gauss Seidel BENCHMARK" 
 echo " no: of threads 72" 
 
-srun \
+srun --cpu-freq=2200000-2200000:performance \
 --export=ALL,OMP_NUM_THREADS=72,OMP_PLACES=cores,OMP_PROC_BIND=close \
 ./benchmark gauss_seidel matrices/HPCG-128-128-128.mtx
 
--- scripts/jacobi.sh
+++ scripts/jacobi.sh
@@ -8,7 +8,7 @@ cd .. && make clean && make
 echo "Jacobi BENCHMARK" 
 echo " no: of threads 72" 
 
-srun \
+srun --cpu-freq=2200000-2200000:performance \
 --export=ALL,OMP_NUM_THREADS=72,OMP_PLACES=cores,OMP_PROC_BIND=close \
 ./benchmark jacobi matrices/HPCG-128-128-128.mtx
 
